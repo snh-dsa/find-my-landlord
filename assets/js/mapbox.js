7ce3f79c853002b3b1cbcc54d9eb68d7ad813b74
snh-dsa <71397379+snh-dsa@users.noreply.github.com>
2020-09-21 02:04:46 -0400

    callout layer

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -70,7 +70,7 @@ map.on("load", function() {
 				"id": "allProperties",
 				"type": "circle",
 				"source": "propertyData",
-				"source-layer": "features",
+				"source-layer": "Ward5-Test-9202020-7",
 				"paint": {
 					"circle-radius": defaultRadius,
 					"circle-color": defaultColors,

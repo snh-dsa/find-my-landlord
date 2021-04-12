e0dc3b3935cb99a16cd42f65fc49e9a4374470b8
not-a-socialist <71472658+not-a-socialist@users.noreply.github.com>
2021-04-12 17:33:48 -0400

    start of api
    anPropReq requests more information about a selected property without contact information

---
+++ assets/js/api.js
@@ -0,0 +1,15 @@
+function anPropReq () {
+	var xhr, url, data, outgoingMessage;
+
+	url = "https://northamerica-northeast1-vivid-alchemy-109421.cloudfunctions.net/apr";
+	
+	data = App_Global_Selected_Feature;
+    data.api = {};
+	data.api.flag = "apr";
+	
+	outgoingMessage = JSON.stringify(data);
+	
+	xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.send(outgoingMessage);
+};
